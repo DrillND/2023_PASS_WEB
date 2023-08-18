504d8d1bccd8ba0c1edb352f085421945479996c
oralcoder <oralcoder@gmail.com>
2023-08-18 16:11:55 +0900

    code for issue #1

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 module.exports = {
     CheckAuth: async function(req, res, next) {
         console.log('로그인 된 사용자입니다.');
+        console.log('로그인 된 사용자입니다.');
+        console.log('로그인 된 사용자입니다.');
         if(req.isAuthenticated()) {
             next();
         }
