8969e842db0db34cb66d1e1b018f0d840a64a768
oralcoder <oralcoder@gmail.com>
2023-08-18 15:41:42 +0900

    modified on a branch

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
     CheckAuth: async function(req, res, next) {
-        console.log('auth');
+        console.log('로그인 된 사용자입니다.');
         if(req.isAuthenticated()) {
             next();
         }
